bc658c8e4f606019b413b3f506df23645f3ee606
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-04 10:39:49 +0100

    looped so they keep talking until stopped

--- client.rb
+++ client.rb
@@ -2,9 +2,11 @@ require 'socket'
 
 socket = TCPSocket.new('localhost', 2345)
 
+loop do
 puts socket.gets
-puts "Wow what a rude server... I'm going to say Sarah"
-socket.puts "Sarah"
+puts "Wow what a rude server... I'm going to say #{name = ["Melani","Yvonne","Usha","Raphael","Opal","Arnetta","Leopoldo","Jamison","Leslie","Antonia","Cleo","Naomi","Livia","Crissy","Louetta","Noelia","Augustine","Tari","Esteban","Rubie","Marlyn","Cherish","Rolland","Jena","Weldon","Lorenzo","Tarsha","Clarisa","April","Solomon","Tai","Agueda","Shelley","Debby","Duane","Son","Leilani","Eileen","Gabriel","Velda","Lia","Lillie","Jack","Elvin","Vaughn","Cristi","Alesha","Ora","Shakira","Avery"].sample}"
+socket.puts "#{name}"
 puts socket.gets
+end
 
 socket.close
